9d8cad14c0e0bc7bb296bcf11a5b1e2c26f67e90
Fahryrasyidinantaputra <if24.fahryputra@mhs.ubpkarawang.ac.id>
2025-09-28 05:02:41 +0700

    Initial commit

---
+++ tugas.py
@@ -0,0 +1,12 @@
+# Queue pakai list
+antrian = []
+
+# Menambahkan (enqueue)
+antrian.append("Orang 1")
+antrian.append("Orang 2")
+antrian.append("Orang 3")
+
+# Mengeluarkan (dequeue)
+print("isi antrian :",antrian)
+print("mengeluarkan :",antrian.pop(0))
+print("isi antrian :",antrian)
